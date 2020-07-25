1df347f90f23dcb6cf9fab689a692ad749bbf901
rirto0627 <47493742+rirto0627@users.noreply.github.com>
2020-07-25 17:02:48 +0800

    demo

--- catalog/views.py
+++ catalog/views.py
@@ -9,13 +9,36 @@ from catalog.models import widget
 # Create your views here.
 def home(request):
     # style從json更改
-    css = 'second'
-    style = f'/css/{css}.css'
-    with open('./templates/setting.json', 'r', encoding='utf8') as jf:
+    # css = 'second'
+    # style = f'/css/{css}.css'
+    css1 = '/css/app.c8413253.css'
+    css2 = '/css/chunk-vendors.35c65227.css'
+    css3 = '/css/chunk-vendors.35c65227.css'
+    css4 = '/css/app.c8413253.css'
+    js1 = '/js/app.84f32b18.js'
+    js2 = '/js/chunk-vendors.c13c749a.js'
+    js3 = '/js/chunk-vendors.c13c749a.js'
+    js4 = '/js/app.84f32b18.js'
+    icon = f"/favicon.ico"
+    with open('./templates/home/config.json', 'r', encoding='utf8') as jf:
         jd = json.load(jf)
-    return render(request, 'home/home.html', {'style': style, 'json': jd})
+    return render(request, 'home/index.html',
+
+                  {
+                      'json': jd,
+                      'css1': css1,
+                      'css2': css2,
+                      'css3': css3,
+                      'css4': css4,
+                      'js1': js1,
+                      'js2': js2,
+                      'js3': js3,
+                      'js4': js4,
+                      'icon': icon
+                  }
+                  )
 
 
-def front(request):
+def config(request):
     # front_list = front.objects.all()
-    return render(request, 'home/index.html', )
+    return render(request, 'home/config.json', )
--- widget/urls.py
+++ widget/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('home/', views.home),
-    path('front/', views.front),
+    path('home/config.json', views.config),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
